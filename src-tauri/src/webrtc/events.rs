71a18f89615557f67dded8e849b00892f554cdce
Paweł Romanowski <pawel.romanowski000@gmail.com>
2023-02-11 22:37:37 +0100

    Payload test

--- src-tauri/src/webrtc/events.rs
+++ src-tauri/src/webrtc/events.rs
@@ -1,12 +1,14 @@
 use std::sync::Arc;
 
 use crate::compiler::compile_with_chanel;
+use crate::webrtc::payload::Payload;
 use webrtc::data_channel::data_channel_message::DataChannelMessage;
 use webrtc::data_channel::RTCDataChannel;
 use webrtc::peer_connection::peer_connection_state::RTCPeerConnectionState;
 
 pub fn on_message(msg: DataChannelMessage, channel: Arc<RTCDataChannel>) {
     let msg_str = String::from_utf8(msg.data.to_vec()).unwrap();
+    //let peyload = Payload::new(msg_str);
     println!(
         "Message from DataChannel {}: '{}'",
         channel.label().to_owned(),
--- src-tauri/src/webrtc/payload.rs
+++ src-tauri/src/webrtc/payload.rs
@@ -69,6 +69,26 @@ impl Display for Payload {
 mod tests {
     use super::Payload;
     use super::PayloadType;
+    use super::HEAD_VARIANT_TYPE_ANSWER;
+    use super::HEAD_VARIANT_TYPE_CODE;
+    use super::HEAD_VARIANT_TYPE_COMMAND;
+    use super::HEAD_VARIANT_TYPE_SYSTEM;
+
+    #[test]
+    fn basic() {
+        let code = r#"
+            variant: code
+            ---BODY---
+            echo ""
+            echo "Test"
+        "#;
+
+        let ob = Payload::new(code.to_string());
+
+        assert_eq!(ob.variant, PayloadType::Code);
+        assert_eq!(ob.raw, code);
+    }
+
     #[test]
     fn empty_head() {
         let code = r#"
@@ -81,5 +101,41 @@ mod tests {
         assert_eq!(ob.variant, PayloadType::Code);
         assert_eq!(ob.body, code);
         assert_eq!(ob.raw, code);
+    }
+
+    #[test]
+    fn heads() {
+        let types = [
+            HEAD_VARIANT_TYPE_CODE,
+            HEAD_VARIANT_TYPE_COMMAND,
+            HEAD_VARIANT_TYPE_ANSWER,
+            HEAD_VARIANT_TYPE_SYSTEM,
+        ];
+
+        let variant = [
+            PayloadType::Code,
+            PayloadType::Command,
+            PayloadType::Answer,
+            PayloadType::System,
+        ];
+
+        assert_eq!(
+            types.len(),
+            variant.len(),
+            "types and variant must have the same length FIX THE TESTS"
+        );
+
+        for index in 0..types.len() {
+            let head = format!("variant: {} ", types[index]);
+            let code = r#"
+                echo ""
+                echo "Test"
+            "#;
+
+            let ob = Payload::new(format!("{} \n ---BODY--- \n {}", head, code));
+
+            assert_eq!(ob.variant, variant[index]);
+            assert_eq!(ob.body.trim(), code.trim());
+        }
     }
 }
